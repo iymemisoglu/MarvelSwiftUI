092e1007bd9acc18cd69e975c7976967a01b68b0
ilker <ilker.memisoglu@icloud.com>
2023-01-29 19:33:30 +0300

    Initial Commit

---
+++ Marvel/MarvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelApp.swift
+//  Marvel
+//
+//  Created by İlker Yasin Memişoğlu on 29.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct MarvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
